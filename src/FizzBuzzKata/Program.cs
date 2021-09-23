a1db955f5849771b4f0086b8a57538a6af4e795d
Guillaume-Riviere <91249622+Guillaume-Riviere@users.noreply.github.com>
2021-09-23 17:02:50 +0200

    Advancement to the project
    Add when
    X = multiple of 3 to display Fizz
    X = Multiple of 5 to display Buzz
    X = Multiple of 3 and 5 to display FizzBuzz

--- src/FizzBuzzKata/Program.cs
+++ src/FizzBuzzKata/Program.cs
@@ -6,7 +6,31 @@ namespace FizzBuzzKata
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i <= 15; i = i + 1)
+            {
+
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                       Console.WriteLine("Fizz"); 
+                    }
+                    
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
         }
     }
 }
